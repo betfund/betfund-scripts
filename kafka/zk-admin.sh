b2c719fba23126de960d4c0a48907551afb18017
leonkozlowski <leonkozlowski@gmail.com>
2020-05-11 19:59:37 -0400

    more formatting

--- kafka/zk-admin.sh
+++ kafka/zk-admin.sh
@@ -17,34 +17,32 @@ KPID="/var/run/$KNAME.pid"
 ZLOG="/var/log/$ZNAME.log"
 KLOG="/var/log/$KNAME.log"
 
-function start-zookeeper {
-	echo "---------------------------"
-	echo ">> Starting Zookeeper..."
-	echo -e "---------------------------"
-	if [ -f "$ZPID" ]; then
-		echo "$ Already running $ZNAME"
-	else
-		sudo sh /opt/kafka_2.13-2.4.0/bin/zookeeper-server-start.sh -daemon config/zookeeper.properties
-	fi
+function start-zookeeper() {
+  echo "---------------------------"
+  echo ">> Starting Zookeeper..."
+  echo -e "---------------------------"
+  if [ -f "$ZPID" ]; then
+    echo "$ Already running $ZNAME"
+  else
+    sudo sh /opt/kafka_2.13-2.4.0/bin/zookeeper-server-start.sh -daemon config/zookeeper.properties
+  fi
 }
 
-function start-kafka {
-	echo -e "\n---------------------------"
-	echo ">> Starting Kafka..."
-	echo "---------------------------"
-	if [ -f "$KPID" ]; then
-		echo "$ Already running $KNAME"
-		exit 1
-	else
-		sudo sh /opt/kafka_2.13-2.4.0/bin/kafka-server-start.sh config/server.properties
-	fi
+function start-kafka() {
+  echo -e "\n---------------------------"
+  echo ">> Starting Kafka..."
+  echo "---------------------------"
+  if [ -f "$KPID" ]; then
+    echo "$ Already running $KNAME"
+    exit 1
+  else
+    sudo sh /opt/kafka_2.13-2.4.0/bin/kafka-server-start.sh config/server.properties
+  fi
 }
 
 case "$1" in
-  start)
-    start-zookeeper
-    start-kafka
-    ;;
-     echo "Usage: /etc/init.d/kafka {start}" && exit 1
-    ;;
+start)
+  start-zookeeper
+  start-kafka
+  ;;
 esac
